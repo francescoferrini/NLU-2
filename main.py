9f99c390cfd2f200bb14964693891eaec6d8aae4
francescoferrini <51090995+francescoferrini@users.noreply.github.com>
2021-04-30 23:41:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ corpus_file = 'test.txt'
 sentences_list = reconstructSentences(corpus_file)
 
 retokenized_list1, retokenized_list2 = retokenizationOfSentences(sentences_list)
-print("----------------------------------------------------------------------------------------------------------")
 gt_list1, gt_list2 = groundTruthList(corpus_file)
 print(classification_report(gt_list1, retokenized_list1))
 
